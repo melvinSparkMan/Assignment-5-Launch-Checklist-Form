b39f7634c9c67344fc5e29fc19a48393b215eecd
melvinSparkMan <melvinsparkman@hotmail.com>
2021-12-02 19:05:27 -0600

    Updated

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
+
+
 window.addEventListener("load", function() {
 
     let listedPlanets;
     // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-    let listedPlanetsResponse;
-    listedPlanetsResponse.then(function (result) {
-        listedPlanets = result;
-        console.log(listedPlanets);
-    }).then(function () {
-        console.log(listedPlanets);
-        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-    
-    
+        let listedPlanetsResponse = myFetch();
+            listedPlanetsResponse.then(function (result) {
+                listedPlanets = result;
+                    console.log(listedPlanets);
+                        }).then(function () {
+                            console.log(listedPlanets);
+        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.       
     })
-    
- });
-
-                            myFetch();
-
+});
                                 pickPlanet();
                             
                                     validateInput();
